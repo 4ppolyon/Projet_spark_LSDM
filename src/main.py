67b5f13eea14cbde6446dd88a9d6d5196f4f03d4
alvesr <romain.alves@etu.univ-grenoble-alpes.fr>
2025-01-05 17:09:56 +0100

    modif import data en fonction des questions

--- src/main.py
+++ src/main.py
@@ -7,21 +7,32 @@ from src.questions import *
 sc = SparkContext("local[1]")
 sc.setLogLevel("ERROR")
 
-# import the data (no header)
-data = sc.textFile("./data/machine_events/*.csv")
+# affiliate the col list below
+machine_event_col = ['timestamp', 'machineID', 'eventtype', 'platformID', 'cpucapacity', 'memorycapacity']
+job_event_col = ['timestamp', 'missinginfo', 'jobID', 'eventtype', 'user', 'schedulingclass', 'jobname', 'logicaljobname']
+task_event_col = ['timestamp', 'missinginfo', 'jobID', 'taskindex', 'machineID', 'eventtype', 'username', 'schedulingclass', 'priority', 'cpu', 'ram', 'disk', 'machineconstraint']
+
+def split_data(data):
+    data = data.map(lambda x: x.split(','))
+    print("Data Loaded", data.count(), "rows")
+    return data
 
 # split the data
-data = data.map(lambda x: x.split(','))
-print("Data Loaded", data.count(), "rows")
-
-# affiliate the col list below
-col = ['timestamp', 'machineID', 'eventtype', 'platformID', 'cpucapacity', 'memorycapacity']
-
-# if we execute the code with the argument "filtered", we will filter the data to remove the rows with missing values
-if len(sys.argv) > 1 and "filtered" in sys.argv:
+def filter_data(data):
     print("Filtering Data to remove rows with missing values (if you want to keep them, do not use the argument 'filtered')")
+    # filter the data to remove the rows with missing values
     data = data.filter(lambda x: all(c != '' for c in x))
     print("Data Filtered", data.count(), "rows remaining")
+    return data
+
+def load_data(name):
+    # concatenate the name with ./data/ and /*.csv
+    data = sc.textFile(f"./data/{name}/*.csv")
+    data = split_data(data)
+    # if we execute the code with the argument "filtered", we will filter the data to remove the rows with missing values
+    if len(sys.argv) > 1 and "filtered" in sys.argv:
+        data = filter_data(data)
+    return data
 
 #####################
 #                   #
@@ -30,10 +41,11 @@ if len(sys.argv) > 1 and "filtered" in sys.argv:
 #####################
 
 def q1():
+    data = load_data("machine_events")
     print("_" * 100,"\nQuestion 1 :")
     print("What is the distribution of the machines according to their CPU capacity?\n")
     start = time.time()
-    res1 = question1(data, col)
+    res1 = question1(data, machine_event_col)
     print("\nExecution Time :", round(time.time() - start, 2), "s\n")
 
     # plot the data
@@ -49,11 +61,12 @@ def q1():
     plt.show()
 
 def q2():
+    data = load_data("machine_events")
     print("_" * 100, "\nQuestion 2 :")
     print("What is the percentage of computational power lost due to maintenance?\n")
 
     start = time.time()
-    res2 = question2(data, col)
+    res2 = question2(data, machine_event_col)
     if res2 == -1:
         print("An error occurred, please check the data.")
     else:
